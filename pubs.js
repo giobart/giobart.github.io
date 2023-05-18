3e43a9c00415b13627af08132810c65ed01e4202
giobarty <giovanni.bartolomeo.rg@gmail.com>
2023-05-18 14:31:28 +0200

    added scholar scheme

--- pubs.js
+++ pubs.js
@@ -8,6 +8,7 @@ fetch('pubs.yaml')
     publications.forEach(publication => {
       const publicationElement = document.createElement('div');
       var supervision = publication.issupervision ? `Role: <span class="badge badge-primary badge-pill">Advisor</span>` : ` `;
+      var link = publication.doi ? `href="${publication.doi}"` : `disabled`;
       publicationElement.innerHTML = `
       <li class="list-group-item bg-dark">
         <div class="card mb-3 bg-dark" style="max-width: 540px;">
@@ -16,13 +17,15 @@ fetch('pubs.yaml')
             <img style="width: 100%;" src="${publication.image}" class="img-fluid rounded-start" alt="publication cover image">
             </div>
             <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title"> <b> ${publication.title} </b></h5>
-                <p class="card-text">Authors: ${publication.author}</p>
-                <p class="card-text">${publication.journal_title}</p>
+            <div class="card-body" itemscope itemtype="https://schema.org/ScholarlyArticle">
+                <h5 class="card-title"> <b> <a itemprop="name" ${link}> ${publication.title} </a> </b></h5>
+                <p class="card-text" >Authors: <span itemprop="author"> ${publication.author} </span></p>
+                <p class="card-text"> <span itemprop="journalName"> ${publication.journal_title} </span></p>
                 ${supervision}
                 <p class="card-text">Pages: ${publication.firstpage}-${publication.lastpage}</p>
-                <p class="card-text"><small class="text-muted">Date: ${publication.publication_date}</small></p>
+                <p class="card-text"><small class="text-muted">Date: <span itemprop="datePublished">${publication.publication_date}</span></small></p>
+                <a itemprop="url" ${publication.doi}></a>
+                <a itemprop="doi" ${publication.doi}></a>
                 <a class="btn btn-primary" href="${publication.pdf_url}" target="_blank"><span class="bi bi-file-pdf"></span>PDF</a>
             </div>
             </div>
