ce9e7ba9d805b9c6b33253e847aed173f1868330
giobarty <giovanni.bartolomeo.rg@gmail.com>
2023-05-18 13:58:21 +0200

    fixed cursor and added bold titles

--- pubs.js
+++ pubs.js
@@ -11,13 +11,13 @@ fetch('pubs.yaml')
       publicationElement.innerHTML = `
       <li class="list-group-item bg-dark">
         <div class="card mb-3 bg-dark" style="max-width: 540px;">
-        <div class="row g-0">
+        <div class="row g-0 align-items-center">
             <div class="col-sm-4">
             <img style="width: 100%;" src="${publication.image}" class="img-fluid rounded-start" alt="publication cover image">
             </div>
             <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">  ${publication.title} </h5>
+                <h5 class="card-title"> <b> ${publication.title} </b></h5>
                 <p class="card-text">Authors: ${publication.author}</p>
                 <p class="card-text">${publication.journal_title}</p>
                 ${supervision}
